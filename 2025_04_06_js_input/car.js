66997e377afc83ee69e1881c91829e1084002128
Kseniia Semenova <ru.kseniass@gmail.com>
2025-06-04 21:45:04 +0300

    инпуты

---
+++ 2025_04_06_js_input/car.js
@@ -0,0 +1,6 @@
+// Написать страницу, которая для любого значения времени и скорости рассчитает расстояние
+
+function start(){
+    let rasstoyanie = speed.valueAsNumber * time.valueAsNumber;
+    console.log(rasstoyanie)
+}
---
+++ 2025_04_06_js_input/javascript.js
@@ -0,0 +1,6 @@
+function start(){
+    console.log('С днём рождения!')
+    //<input type='number' id="year" placeholder = 'Год рождения'>
+    let age = 2025 - year.valueAsNumber;
+    console.log(age)
+}
