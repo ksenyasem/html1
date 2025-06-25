47e56d2319d141b823d06fca6b1273f250444d53
Kseniia Semenova <ru.kseniass@gmail.com>
2025-06-25 22:02:10 +0300

    позиционные массивы

---
+++ 2025_06_25(2)/arr_test.js
@@ -0,0 +1,8 @@
+const imya = 'Вася'
+// Буквы нумеруются с нуля
+// Задание: вывести все буквы имени
+let bukva_nomer = 0
+while (bukva_nomer <4 ) {
+    console.log(imya[bukva_nomer])
+    bukva_nomer+=2
+}
